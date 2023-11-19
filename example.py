aa57297eeed8f34597db0b9c8604174d1c53fca2
Kirya <116507003+tembz@users.noreply.github.com>
2023-11-19 13:07:51 +0300

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import asyncio
 hella = HAPI(Validate("HELLA KEY"))
 
 async def test():
-    get_stickers = await hella.getStickers(user_id=497625184)
+    get_stickers = await hella.get_stickers(user_id=497625184)
     print(get_stickers.object.items)
 
 asyncio.run(test())
