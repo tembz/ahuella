bb4325cb20bdae35bb2aca5db7d1eb0c7c35d57d
Kirya <116507003+tembz@users.noreply.github.com>
2024-02-16 17:35:08 +0300

    Update tools.py

--- ahuella/tools.py
+++ ahuella/tools.py
@@ -12,7 +12,8 @@ class Validate(str):
 	def __new__(cls, token: str, v: int = 2) -> str | Exception:
 		from .hellahttp import Client
 		if v in versions:
-			is_valid = asyncio.run(Client().get_token(f'https://api.hella.team/method/ping?v=1&access_token={token}'))
+			loop = asyncio.get_event_loop()
+			is_valid = loop.run_until_complete(Client().get_token(f'https://api.hella.team/method/ping?v=1&access_token={token}'))
 			if is_valid['ok'] == True:
 				logging.debug(f"Validation successful! API version used - {v}")
 				return super().__new__(cls, f"{token}:{v}")
